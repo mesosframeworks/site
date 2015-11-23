2bb92e820d2eff513a6fca26ec31ea8c052bac6b
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2015-11-23 17:30:57 +0100

    Created main view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>RTP Minisites CMS</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/style.min.css">
+		<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+		
+    </head>
+    <body>
+        
+        <p>Hello world! This is RTP Minisites CMS.</p>		
+		
+		<script src="js/bundle.min.js"></script>
+
+    </body>
+</html>
