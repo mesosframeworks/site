5a671a1a51d575165144072def8ea0aa63d642e0
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2016-02-01 23:26:01 +0100

    Updated to Laravel 5.2

--- resources/views/sites/components/listitem.blade.php
+++ resources/views/sites/components/listitem.blade.php
@@ -1,13 +1,25 @@
 <div class="row">
+	
     <div class="columns small-12">
+    
         <div class="inner @if ($index === 0) no-border @endif">
+	
 	        <div class="columns small-3 medium-2 medium-offset-1 text-right">
+	
 				<img src="uploads/sites/{{ $id }}/{{ $item->icon }}">
+	
 	        </div>
+	
 	        <div class="columns small-9 medium-8 end">
-		        <h3 class="heading decoration line-below">{{ $item->title }}</h3>
-		        <p>{{ $item->content }}</p>
+	
+		        <h3 class="heading decoration line-below">{{ $item->title or '' }}</h3>
+	
+		        <p>{{ $item->content or '' }}</p>
+	
 	        </div>
+    
         </div>
+    
     </div>
+
 </div>
