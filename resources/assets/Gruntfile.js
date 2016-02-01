4e1f4b66069353d0a4c4e956b87f2b90f080b9de
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2016-02-01 19:54:40 +0100

    Fixed gruntfile

--- resources/assets/Gruntfile.js
+++ resources/assets/Gruntfile.js
@@ -4,48 +4,144 @@ module.exports = function(grunt) {
 		
 		pkg: grunt.file.readJSON("package.json"),
 		
+		watch: {
+			
+			scriptsadmin : {
+				files: ["js/*admin.js"],
+				tasks: ["uglify:admin"]
+			},
+			scriptssites : {
+				files: ["js/*sites.js"],
+				tasks: ["uglify:sites"]
+			},
+/*
+			scriptsrepository : {
+				files: ["js/*repository.js"],
+				tasks: ["uglify:repository"]
+			},
+			scriptslogoGenerator : {
+				files: ["scss/logoGenerator.scss"],
+				tasks: ["sass:logoGenerator"]
+			},
+*/
+			
+			stylesadmin : {
+				files: "**/*.scss",
+			    tasks: ["sass:admin"]		
+			},
+			stylessites : {
+				files: ["scss/*.scss"],
+			    tasks: ["sass:sites"]
+			},
+/*
+			stylesrepository : {
+				files: ["scss/repository.scss"],
+				tasks: ["sass:repository"]
+			},
+			styleslogoGenerator: {
+				files: ["scss/logoGenerator.scss"],
+				tasks: ["sass:logoGenerator"]	
+			}
+*/		
+		},
+		
 		sass : {
-			dist : {
+			admin : {
 				options: {                      
 					style: "compressed"
       			},
 				files : {
-					"../../public/css/style.min.css" : "scss/app.scss",
-					"../../public/css/admin.min.css" : "scss/admin.scss",
+					"../../public/css/admin.min.css" : "scss/admin.scss"
+				}
+			},
+			sites : {
+				options: {                      
+					style: "compressed"
+      			},
+				files : {
 					"../../public/css/sites.min.css" : "scss/sites.scss"
 				}
+			}/*
+,
+			repository : {
+				options: {                      
+					style: "compressed"
+      			},
+				files : {
+					"../../public/css/repository.min.css" : "scss/repository.scss"
+				}
+			},
+			logoGenerator : {
+				options: {                      
+					style: "compressed"
+      			},
+				files : {
+					"../../public/css/logoGenerator.min.css" : "scss/logoGenerator.scss"
+				}
 			}
-		},
-		
-		watch : {
-			css: {
-				files : "**/*.scss",
-				tasks : ["sass"],
-				livereload: true // TODO: Make this work
-			}
+*/
 		},
 		
 		bower_concat : {
 			
-			all : {
-				dest : "js/bundle.js",
+			admin : {
+				dest : "js/temp/bundle-admin.js",
 				exclude : ["modernizr"],
-				include : ["jquery", "foundation", "dropzone", "jquery.cycle2.min", "jquery.scrollTo"]
+				include : ["jquery", "foundation", "dropzone"]
+			},
+			sites : {
+				dest : "js/temp/bundle-sites.js",
+				exclude : ["modernizr"],
+				include : ["jquery", "foundation", "jquery.cycle2.min", "jquery.scrollTo"]
+			},
+			repository : {
+				dest : "js/temp/bundle-repository.js",
+				include : ["jquery"]
+			},
+			logoGenerator : {
+				dest : "js/temp/bundle-logoGenerator.js",
+				exclude : ["modernizr"],
+				include : ["jquery", "foundation"]
 			}
 		},
 		
 		uglify : {
-			bower : {
+			admin : {
 				options: {
 					mangle : true,
 					compress : true
     			},
 				files : {
-					"../../public/js/bundle.min.js" : ["js/bundle.js", "js/main.js"],
-					"../../public/js/sites.min.js" : ["js/bundle.js", "js/sites.js"],
-					"../../public/js/admin.min.js" : ["js/bundle.js", "js/admin.js"]
+					"../../public/js/admin.min.js" : ["js/temp/bundle-admin.js", "js/admin.js"]
+    			}	
+			},
+			sites : {
+				options: {
+					mangle : true,
+					compress : true
+    			},
+				files : {
+					"../../public/js/sites.min.js" : ["js/temp/bundle-sites.js", "js/sites.js"]
     			}
-  			}
+			},
+			repository : {
+				options: {
+					mangle : true,
+					compress : true
+    			},
+				files : {
+					"../../public/js/repository.min.js" : ["js/temp/bundle-repository.js", "js/repository.js"]
+    			}
+			},
+			logoGenerator : {
+				options: {
+					mangle : true,
+					compress : true
+    			},
+				files : {
+					"../../public/js/logoGenerator.min.js" : ["js/bundle-logoGenerator.js", "js/logoGenerator.js"]
+    			}
+			}
 		}
 	});
 	
@@ -54,6 +150,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-sass");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 	
-	grunt.registerTask("buildstyles",["watch"]);
-	grunt.registerTask("buildscripts", ["bower_concat", "uglify:bower"]);
+	grunt.registerTask("build", ["bower_concat", "uglify", "sass"]);
+	
+	grunt.registerTask("buildadmin", ["bower_concat:admin", "uglify:admin"]);	
+	grunt.registerTask("buildsites", ["bower_concat:sites", "uglify:sites"]);	
+	grunt.registerTask("buildrepository", ["bower_concat:repository", "uglify:repository"]);	
+	grunt.registerTask("buildlogoGenerator", ["bower_concat:logoGenerator", "uglify:logoGenerator"]);
 };
--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -2,3 +2,4 @@ var foo = function() {
 	
 	//console.log("SITESISTES");
 }
+console.log('Hola Mundo');
