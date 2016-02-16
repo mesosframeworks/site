45edb35398a5c2c7274bb819046b3771150c7aaf
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2016-02-16 18:32:16 +0100

    Added prism.scss back

--- resources/views/admin/sites.blade.php
+++ resources/views/admin/sites.blade.php
@@ -86,7 +86,7 @@
 			$("." + res.site_id).hide('fast');
 			$(".no-sites").hide('fast');
 			
-			$("#sites").prepend("<div class='columns small-6 medium-4 " + res.site_id + "'><div class='site-thumb'><img src='uploads/sites/" + res.site_id + "/logo.svg'><div class='controls'><ul><li><a href='' class='download' title='Download site'><i class='material-icons'>get_app</i></a></li><li><a href='/admin/site/status/" + res.id + "' class='public' title='Publish site'><i class='material-icons'>turned_in_not</i></a></li><li><a href='http://" + res.site_id + ".rtp-cms.dev' class='preview' title='Preview'><i class='material-icons'>search</i></a></li><li><a href='/admin/site/delete/" + res.id + "' class='delete_site' title='Delete site'><i class='material-icons'>delete</i></a></li></ul><p><b>Created at</b>" + res.created_at + "</p><p><b>Last updated</b>" + res.updated_at + "</p></div></div></div>");
+			$("#sites").prepend("<div class='columns small-6 medium-4 " + res.site_id + "'><div class='site-thumb'><img src='uploads/sites/" + res.site_id + "/logo.svg'><div class='controls'><ul><a href='/admin/site/status/" + res.id + "' class='public' title='Publish site'><i class='material-icons'>turned_in_not</i></a></li><li><a href='http://" + res.site_id + ".rtp-cms.dev' class='preview' title='Preview'><i class='material-icons'>search</i></a></li><li><a href='/admin/site/delete/" + res.id + "' class='delete_site' title='Delete site'><i class='material-icons'>delete</i></a></li></ul><p><b>Created at</b>" + res.created_at + "</p><p><b>Last updated</b>" + res.updated_at + "</p></div></div></div>");
 			}
 	};
 
